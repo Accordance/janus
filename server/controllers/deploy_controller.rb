1aae1c1a024a4b1efd42465974cd05cdf98d91b7
xkarlak <karunkarreddy.l@tcs.com>
2016-01-22 17:50:11 +0530

    Modified code to send json object to Atlas

--- server/controllers/deploy_controller.rb
+++ server/controllers/deploy_controller.rb
@@ -18,7 +18,12 @@ module API
         [403, 'Deploy Not Allowed']
       ]  do
         
-        response = Faraday.post("http://192.168.99.100:8000/apps/deploy/#{params[:id]}")
+        conn = Faraday.new(:url => 'http://192.168.99.100:8000')
+            response = conn.post do |req| 
+              req.url '/apps/deploy'
+              req.headers['Content-Type'] = 'application/json'
+              req.body = "{ \"id\": \"#{params[:id]}\", \"version\" : \"#{params[:version]}\" }"
+            end
         if response.status == 200
           status 200
         elsif response.status == 403
@@ -31,11 +36,16 @@ module API
         [403, 'Lock release failed']
       ]	do
         
-        response = Faraday.post("http://192.168.99.100:8000/apps/release-deploy/#{params[:id]}")
+        conn = Faraday.new(:url => 'http://192.168.99.100:8000')
+        response = conn.post do |req| 
+          req.url '/apps/release-deploy'
+          req.headers['Content-Type'] = 'application/json'
+          req.body = "{ \"id\": \"#{params[:id]}\", \"version\" : \"#{params[:version]}\" }"
+        end
         if response.status == 200
           status 200
         elsif response.status == 403
-          error! "Lock could not be released", 403
+          error! "Lock is not Active", 403
         end
       end
     end
